12b581003865dc4b22c6b4a00169302d4e3249d0
in24 <54269634+in24@users.noreply.github.com>
2020-01-10 15:22:16 +0200

    lasa

--- static/chats.js
+++ static/chats.js
@@ -1,1 +1,18 @@
-console.log("Sveiciens!");
+const ATJAUNOT = 5000; // atjauno ik pēc 5 sekundēm
+
+async function lasiChatu() {
+  // JavaScriptā tā var apstrādāt pieprasījumu, kuram atnāk atbilde no servera
+  const atbilde = await fetch("/chats/lasi"); // await strādā tikai asinc
+  const datuObjekts = await atbilde.json();
+  raadiChatuVienkarsi(datuObjekts);
+}
+function raadiChatuVienkarsi(dati) {
+  const jaunaRinda = "</br>";
+  let chats = "";
+  let chataDiv = document.getElementById("chats");
+  for (let rinda of dati["chats"]) {
+    chats = chats + rinda + jaunaRinda;
+  }
+  chataDiv.innerHTML = chats;
+}
+function suutiZinju() {}
