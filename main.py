86d9da5c964b0c3d6cffd7139902d7f08274ba48
in24 <54269634+in24@users.noreply.github.com>
2020-01-10 14:32:46 +0200

    lasīt un sūtīt

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json, jsonify, request
 
 
 app = Flask('app')
@@ -6,11 +6,30 @@ app = Flask('app')
 
 @app.route('/')
 def index_page():
-  return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/veseliba')
 def veseliba():
-  return "OK"
+    return "OK"
+
+
+@app.route('/chats/lasi')
+def ielasit_chatu():
+    chata_rindas = []
+    with open("chats.txt", "r", encoding="UTF-8") as f:
+        for rinda in f:
+            chata_rindas.append(rinda)
+
+    return jsonify({"chats": chata_rindas})
+
+
+@app.route('/chats/suuti', methods=['POST'])
+def suuti_zinju():
+    dati = request.json
+    with open("chats.txt", "a", encoding="UTF-8") as f:
+        f.write(dati["chats"]+"\n")
+    return ielasit_chatu()
 
 
 if __name__ == '__main__':
